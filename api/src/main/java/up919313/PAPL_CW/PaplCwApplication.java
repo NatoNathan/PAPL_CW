759b962d7bf146258876e3df05067d65ba052397
Nathan Tamez <jamestamezn@gmail.com>
2021-01-09 00:03:53 +0000

    made a start

---
+++ api/src/main/java/up919313/PAPL_CW/PaplCwApplication.java
@@ -0,0 +1,14 @@
+package up919313.PAPL_CW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.*;
+
+@SpringBootApplication
+public class PaplCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaplCwApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/up919313/PAPL_CW/PaplCwApplicationTests.java
@@ -0,0 +1,13 @@
+package up919313.PAPL_CW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaplCwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
